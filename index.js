bfb6428fa0f2dfa6da22d20b536122b45b950713
mmagc <matheusgnann@gmail.com>
2025-01-14 08:27:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let XP = 10010;
+
+if (XP < 1000){
+  console.log("Ferro");
+} else if (XP >= 1001 && XP <= 2000) {
+  console.log("Bronze");
+} else if (XP >= 2001 && XP <= 5000) {
+  console.log("Prata");
+} else if (XP >= 5001 && XP <= 7000) {
+  console.log("Ouro");
+} else if (XP >= 7001 && XP <= 8000) {
+  console.log("Platina");
+} else if (XP >= 8001 && XP <= 9000) {
+  console.log("Ascendente");
+} else if (XP >= 9001 && XP <= 10000) {
+  console.log("Imortal");
+} else {
+  console.log("Radiante")
+}
+
+console.log("Fim do código");
